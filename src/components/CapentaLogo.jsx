05baa9c3b309341f8316e5653f6528f9bd15eae9
Klas Mellbourn <klas@mellbourn.net>
2018-10-26 18:35:20 +0200

    rotating logo FTW

--- src/components/CapentaLogo.jsx
+++ src/components/CapentaLogo.jsx
@@ -18,13 +18,23 @@ const arc = (size, radius, startDegrees, endDegrees) => {
 
 const CapentaLogo = ({ size }) => {
   const scale = value => ((value * size) / 300).toFixed(3);
+  const cx = size / 2;
+  const cy = size / 2;
   return (
     <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
-      <circle fill="black" r={size / 2} cx={size / 2} cy={size / 2} />
+      <circle fill="black" r={size / 2} cx={cx} cy={cy} />
       <g fill="transparent" strokeLinecap="round" strokeWidth={scale(20)} stroke="yellow">
         {arc(size, scale(40), 0, 160)}
         {arc(size, scale(70 + 4), 60, 220)}
         {arc(size, scale(100 + 8), 120, 270)}
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from={`0 ${cx} ${cy}`}
+          to={`360 ${cx} ${cy}`}
+          dur="180s"
+          repeatCount="indefinite"
+        />
       </g>
     </svg>
   );
